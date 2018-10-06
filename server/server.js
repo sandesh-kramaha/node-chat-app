3dc9c69138a0e5ada6ccd183a3c063eef796f704
sandesh <sandeshsandy325@gmail.com>
2018-10-06 22:21:52 +0530

    add connect and discconect event handlers

--- server/server.js
+++ server/server.js
@@ -8,7 +8,14 @@ const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
+var io = socketIO(server);
+io.on('connection',function(socket){
+  console.log("new user connected");
+  socket.on('disconnect',() => {
+    console.log('user disconnected !!!!');
+  });
 
+});
 
 
 app.use(express.static(publicPath));
