37908a07b1941b1920e63099a96b98d84b2643a4
sandesh <sandeshsandy325@gmail.com>
2018-10-06 17:29:06 +0530

    Add connect

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+
+
 
 app.use(express.static(publicPath));
-app.listen(port,function(){
+server.listen(port,function(){
   console.log(`server starts with port ${port}`);
 });
 
